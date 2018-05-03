a73ed02ef8d3712e3f265ed066ec26bf9707e0c6
thwips <869802+thwips@users.noreply.github.com>
2018-05-02 23:40:57 -0700

    Adding rating stars
    Changed size of restaurant name and added rating image system

--- client/app.js
+++ client/app.js
@@ -6,12 +6,15 @@ const resultsComponent = {
       <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="restaurant in results">
         <div class="card" style="margin-top: 20px;">
           <img class="card-img-top" :src=restaurant.image_url height="180px" width="245px">
-          <div class="card-block">
-            <h4 class="card-title text-center">{{restaurant.name}}</h4>
-            <p class="card-text text-center">{{restaurant.rating}} Stars</p>
-            <center><button v-on:click="" class="btn btn-primary" type="submit">
-              Reviews
-            </button><center>
+          <div class="card-body">
+            <p class="card-title text-center">{{restaurant.name}}</p>
+            <center>
+              <img :src="'/rating/' + restaurant.rating +'.png'" width="90%"><br>
+            </center></div><div class="card-body center-block"><center>
+              <button v-on:click="" class="btn btn-primary" type="submit">
+                Reviews
+              </button>
+            </center>
           </div>
         </div>
       </div>  
