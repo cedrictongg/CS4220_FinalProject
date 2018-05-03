75734c48097161cd4d3db53f0df349792f1c915d
Kevin Tong <kcr1230@hotmail.com>
2018-05-02 19:50:43 -0700

    Dev setup
    eslint, editorconfig

--- client/app.js
+++ client/app.js
@@ -1,36 +1,35 @@
 const resultsComponent = {
-  //TODO: WORK ON THIS
-  template: `<span>{{results}}</span>`,
+  // TODO: WORK ON THIS
+  template: '<span>{{ results }}</span>',
   props: ['results']
-}
+};
 
-const socket = io()
+const socket = io();
 const app = new Vue({
   el: '#yelp-search',
   data: {
     cuisine: '',
     location: '',
-    results: []
+    results: [],
   },
   methods: {
-    searchFoods: function() {
-      if (!this.location)
-        return
+    searchFoods() {
+      if (!this.location) { return; }
 
-      socket.emit('search-foods', {cuisine: this.cuisine, location: this.location})
+      socket.emit('search-foods', { cuisine: this.cuisine, location: this.location });
     }
   },
   components: {
     'results-component': resultsComponent
   }
-})
+});
 
-socket.on('successful-search', terms => {
+socket.on('successful-search', (terms) => {
   // console.log(terms)
   // app.searchResults.push(terms)
   // console.log(app.searchResults)
-  terms.forEach(items => {
-    app.results.push(items)
-  })
-  console.log(app.results)
-})
+  terms.forEach((items) => {
+    app.results.push(items);
+  });
+  console.log(app.results);
+});
--- server/socket.js
+++ server/socket.js
@@ -1,40 +1,39 @@
-module.exports = server => {
-  const
-    io = require('socket.io')(server),
-    moment = require('moment'),
-    config = require('../config')
-    axios = require('axios')
+module.exports = (server) => {
+  const io = require('socket.io')(server);
+  const moment = require('moment');
+  const config = require('../config');
+  const axios = require('axios');
 
-  let searches = []
+  const searches = [];
 
-  io.on('connection', socket => {
-    socket.on('search-foods', terms => {
+  io.on('connection', (socket) => {
+    socket.on('search-foods', (terms) => {
       const searchTerms = {
         cuisine: terms.cuisine,
         location: terms.location,
         time: moment(new Date()).format('h:mm a')
-      }
+      };
 
       axios.get(config.url, {
         headers: {
           'X-Requested-With': 'XMLHttpRequest',
-          'Authorization': `Bearer ${config.api_key}`
+          Authorization: `Bearer ${config.api_key}`
         },
         params: {
           location: terms.location,
           term: terms.cuisine
         }
       })
-      .then(response => {
-        console.log(response['data']['businesses'])
-        businessData = response['data']['businesses']
-        io.emit('successful-search', businessData)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-      searches.push(searchTerms)
+        .then((response) => {
+          console.log(response.data.businesses);
+          const businessData = response.data.businesses;
+          io.emit('successful-search', businessData);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      searches.push(searchTerms);
       // io.emit('successful-search', searchTerms)
-    })
-  })
-}
+    });
+  });
+};
