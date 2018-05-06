23c92172ddedaf0609aff85da4083f32cb1f263b
Kevin Tong <kcr1230@hotmail.com>
2018-05-06 15:05:25 -0700

    Final

--- client/app.js
+++ client/app.js
@@ -1,28 +1,27 @@
 const resultsComponent = {
-  // TODO: WORK ON THIS
   template: `
-  <div class="container">
+  <div class="container text-center">
     <div class="row">
       <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="restaurant in results">
         <div class="card" style="margin-top: 20px;">
-          <img class="card-img-top" :src=restaurant.image_url height="180px" width="245px">
           <div class="card-body">
-            <p class="card-title text-center">{{restaurant.name}}</p>
-            <p class="text-center">{{restaurant.distance}}</p>
-            <center>
-              <img :src=restaurant.rating_url width="90%"><br>
-            </center>
+            <p class="card-title"><strong>{{ restaurant.name }}</strong></p>
           </div>
           <div class="card-body center-block">
-            <center>
             <details>
-              <p>{{restaurant.location}}</p>
-              <p>{{restaurant.phone}}</p>
-              <p>Reviews: {{restaurant.review_count}}</p>
-              <p>{{restaurant.categories}}</p>
+            <summary><strong>Details</strong></summary>
+            <br/>
+            <div><img class="card-img-top" :src=restaurant.image_url height="180px" width="245px"></div>
+            <br/>
+            <div><img :src=restaurant.rating_url width="90%"></div>
+            <br/>
+            <p><strong>{{ restaurant.distance }}</strong></p>
+              <p><strong>{{ restaurant.location }}</strong></p>
+              <p><strong>{{ restaurant.phone }}</strong></p>
+              <p><strong>Reviews: {{ restaurant.review_count }}</strong></p>
+              <p><strong>{{ restaurant.categories }}</strong></p>
               <button v-on:click='searchReviews(restaurant)' class="btn btn-primary" type="submit">Reviews</button>
               </details>
-            </center>
           </div>
         </div>
       </div>
@@ -30,10 +29,10 @@ const resultsComponent = {
   </div>`,
   props: ['results'],
   methods: {
-    searchReviews(restaurant){
+    searchReviews(restaurant) {
       // if (this.results.length == 0) {return}
-      console.log('running searchReviews')
-      this.$parent.searchReviews(restaurant)
+      console.log('running searchReviews');
+      this.$parent.searchReviews(restaurant);
       // socket.emit('search-reviews', restaurant)
     },
   },
@@ -43,12 +42,12 @@ const reviewsComponent = {
   template: `
   <div class="container">
     <div class="jumbotron">
-    <center>
+    <div class="text-center">
      <img class="rounded" :src=selected.image_url height="300px" width="300px">
      <h2>{{selected.name}}</h2>
      <h5>{{selected.location}}</h4>
      <h6>{{selected.categories}}</h6>
-    </center>
+    </div>
     </div>
     <div class="jumbotron" style="margin-bottom:2px;">
       <ul>
@@ -57,14 +56,17 @@ const reviewsComponent = {
     </div>
   </div>
   `,
-  props:['reviews','selected']
-}
+  props: ['reviews', 'selected']
+};
 
 const historyComponent = {
   template: `<div class='nav flex-lg-column flex-row'>
                <ul class='list-unstyled' v-for='searches in history'>
                  <li>
-                   <button class='btn btn-link' v-on:click='returnSearch(searches)'>{{searches.cuisine}} in {{searches.location}} at {{searches.time}}</button>
+                   <button type="button" class='list-group-item list-group-item-action' 
+                   v-on:click='returnSearch(searches)'>
+                   <strong>{{searches.cuisine}} in {{searches.location}} at {{searches.time}}</strong>
+                   </button>
                  </li>
                </ul>
              </div>
@@ -72,10 +74,14 @@ const historyComponent = {
   props: ['history'],
   methods: {
     returnSearch(redo) {
-      socket.emit('redo-search', { cuisine: redo.cuisine, location: redo.location, limit: redo.limit})
+      socket.emit('redo-search', {
+        cuisine: redo.cuisine,
+        location: redo.location,
+        limit: redo.limit
+      });
     }
   }
-}
+};
 
 const socket = io();
 const app = new Vue({
@@ -83,7 +89,7 @@ const app = new Vue({
   data: {
     cuisine: '',
     location: '',
-    limit : '',
+    limit: '',
     results: [],
     selected: {},
     history: [],
@@ -91,49 +97,57 @@ const app = new Vue({
   },
   methods: {
     searchFoods() {
-      if (!this.location) { return; }
-      console.log('running searchFoods()')
-      if(!this.limit)
-        this.limit = 5
-      socket.emit('search-foods', { cuisine: this.cuisine, location: this.location, limit: this.limit })
+      if (!this.location) {
+        return;
+      }
+
+      console.log('running searchFoods()');
+      if (!this.limit) {
+        this.limit = 5;
+      }
+
+      socket.emit('search-foods', {
+        cuisine: this.cuisine,
+        location: this.location,
+        limit: this.limit,
+      });
     },
-    searchReviews(restaurant){
-      app.selected = restaurant
-      console.log(`running search on ${app.selected.name}`)
-      socket.emit('search-reviews', app.selected.id)
+    searchReviews(restaurant) {
+      app.selected = restaurant;
+      console.log(`running search on ${app.selected.name}`);
+      socket.emit('search-reviews', app.selected.id);
     }
   },
   components: {
     'results-component': resultsComponent,
     'reviews-component': reviewsComponent,
-    'history-component': historyComponent
+    'history-component': historyComponent,
   }
 });
 
 socket.on('successful-search', (terms) => {
-  app.results = terms
-  app.reviews = []
-  //console.log(app.results)
-  console.log("business search finished!")
+  app.results = terms;
+  app.reviews = [];
+  // console.log(app.results)
+  console.log('business search finished!');
 });
 
-socket.on('successful-reviews', (reviewData) =>{
+socket.on('successful-reviews', (reviewData) => {
+  app.reviews = reviewData;
+  app.results = [];
+  // console.log(app.selected)
+  // console.log(app.phimage)
+  reviewData.forEach((review) => {
+    console.log('REVIEW DATA: ' + review.text);
+  });
+});
 
-  app.reviews = reviewData
-  app.results = []
-  //console.log(app.selected)
-  //console.log(app.phimage)
-  reviewData.forEach(review =>{
-    console.log('REVIEW DATA: ' + review.text)
-  })
-})
-
-socket.on('search-history', searches => {
-  app.history = []
-  searches.forEach(items => {
+socket.on('search-history', (searches) => {
+  app.history = [];
+  searches.forEach((items) => {
     if (!app.history.includes(items)) {
-      app.history.push(items)
+      app.history.push(items);
     }
-  })
-  console.log("current history: "+app.history)
-})
+  });
+  console.log('current history: ' + app.history);
+});
--- server/socket.js
+++ server/socket.js
@@ -7,13 +7,12 @@ module.exports = (server) => {
   const searches = [];
 
   io.on('connection', (socket) => {
-
     socket.on('search-foods', (terms) => {
       const searchTerms = {
         cuisine: terms.cuisine,
         location: terms.location,
         limit: terms.limit,
-        time: moment(new Date()).format('h:mm a')
+        time: moment(new Date()).format('h:mm a'),
       };
 
       // if (searches.length > 0) {
@@ -23,13 +22,15 @@ module.exports = (server) => {
       //     } else {
       //       console.log('run 1')
       //       searches.push(searchTerms)
-      //       console.log("pushing: " + searchTerms.cuisine + " location :" + searchTerms.location + " time:" + searchTerms.time)
+      //       console.log("pushing: " + searchTerms.cuisine + " location :" +
+      // searchTerms.location + " time:" + searchTerms.time)
       //       io.emit('search-history', searches)
       //     }
       //   })
       // } else {
       //   console.log('run 2')
-      //   console.log("pushing: " + searchTerms.cuisine + " location :" + searchTerms.location + " time:" + searchTerms.time)
+      //   console.log("pushing: " + searchTerms.cuisine + " location :" +
+      // searchTerms.location + " time:" + searchTerms.time)
       //   searches.push(searchTerms)
       //   io.emit('search-history', searches)
       // }
@@ -42,45 +43,45 @@ module.exports = (server) => {
         params: {
           location: terms.location,
           term: terms.cuisine,
-          limit: terms.limit
+          limit: terms.limit,
         }
       })
-      .then((response) => {
-        console.log("emit successful-search")
-        searches.push(searchTerms)
-        io.emit('search-history', searches)
-        const businessData = formatResultsList(response.data.businesses)
-        io.emit('successful-search', businessData);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+        .then((response) => {
+          console.log('emit successful-search');
+          searches.push(searchTerms);
+          io.emit('search-history', searches);
+          const businessData = formatResultsList(response.data.businesses);
+          io.emit('successful-search', businessData);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     });
 
 
-    socket.on('search-reviews', (id) =>{
+    socket.on('search-reviews', (id) => {
       axios.get(`${config.url}${id}/reviews`, {
         headers: {
           'X-Requested-With': 'XMLHttpRequest',
           Authorization: `Bearer ${config.api_key}`
         }
       })
-      .then((response) =>{
-
-        console.log("reviews!")
-        const reviewData = response.data.reviews
-        reviewData.forEach(review =>{
-          if(review.user.image_url == null)
-            review.user.image_url = `http://via.placeholder.com/75?text=${review.user.name}`
+        .then((response) => {
+          console.log('reviews!');
+          const reviewData = response.data.reviews;
+          reviewData.forEach((review) => {
+            if (review.user.image_url == null) {
+              review.user.image_url = `http://via.placeholder.com/75?text=${review.user.name}`;
+            }
+          });
+          io.emit('successful-reviews', reviewData);
         })
-        io.emit('successful-reviews', reviewData)
-      })
-      .catch((error) =>{
-        console.error(error)
-      })
-    })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
 
-    socket.on('redo-search', redo => {
+    socket.on('redo-search', (redo) => {
       axios.get(config.url_search, {
         headers: {
           'X-Requested-With': 'XMLHttpRequest',
@@ -92,71 +93,71 @@ module.exports = (server) => {
           limit: redo.limit
         }
       })
-      .then((response) => {
-        const businessData = formatResultsList(response.data.businesses)
-        io.emit('successful-search', businessData);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    })
-
+        .then((response) => {
+          const businessData = formatResultsList(response.data.businesses);
+          io.emit('successful-search', businessData);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
   });
 };
 
-const formatResultsList = results => {
-  const formatedResults = []
+const formatResultsList = (results) => {
+  const formatedResults = [];
   const defaultFields = {
-    id : "",
-    image_url : "http://via.placeholder.com/245x180",
-    name : "Default Name",
-    rating_url : "/rating/0.png",
-    location : "",
-    phone : "",
-    review_count : 0,
-    distance : "0 KM",
-    categories: ""
-  }
+    id: '',
+    image_url: 'http://via.placeholder.com/245x180',
+    name: 'Default Name',
+    rating_url: '/rating/0.png',
+    location: '',
+    phone: '',
+    review_count: 0,
+    distance: '0 KM',
+    categories: ''
+  };
 
-  results.forEach(restaurant => {
-    const result = Object.assign({}, defaultFields)
+  const hasUndefined = (...args) => args.some(arg => arg === undefined);
 
-    result.id = restaurant.id
+  results.forEach((restaurant) => {
+    const result = Object.assign({}, defaultFields);
 
-    if(!hasUndefined(restaurant.image_url)){
-      result.image_url = restaurant.image_url
-    }
-    if(!hasUndefined(restaurant.name)){
-      result.name = restaurant.name
-    }
-    if(!hasUndefined(restaurant.rating)){
-      result.rating_url = `/rating/${restaurant.rating}.png`
-    }
-    if(!hasUndefined(restaurant.location.address1, restaurant.location.city, restaurant.location.state, restaurant.location.zip_code)){
-      result.location = `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state}, ${restaurant.location.zip_code}`
-    }
-    if(!hasUndefined(restaurant.phone)){
-      result.phone = restaurant.phone
-    }
-    if(!hasUndefined(restaurant.review_count)){
-      result.review_count = restaurant.review_count
-    }
-    if(!isNaN(restaurant.distance)){
-      result.distance = `${(parseFloat(restaurant.distance)/1000).toFixed(2)} KM`
-    }
-    if(!hasUndefined(restaurant.categories, restaurant.price)){
-      result.categories += restaurant.price
-      restaurant.categories.forEach( category => {
-        result.categories += ` - ${category.title}`
-      })
-    }
-    formatedResults.push(result)
-  })
-  return formatedResults
-}
+    result.id = restaurant.id;
 
-const hasUndefined = (...args) => {
-  return args.some( arg => {
-    return arg === undefined
-  })
-}
+    if (!hasUndefined(restaurant.image_url)) {
+      result.image_url = restaurant.image_url;
+    }
+    if (!hasUndefined(restaurant.name)) {
+      result.name = restaurant.name;
+    }
+    if (!hasUndefined(restaurant.rating)) {
+      result.rating_url = `/rating/${restaurant.rating}.png`;
+    }
+    if (!hasUndefined(
+      restaurant.location.address1, restaurant.location.city,
+      restaurant.location.state, restaurant.location.zip_code
+    )) {
+      result.location = `${restaurant.location.address1}, ${restaurant.location.city}, 
+      ${restaurant.location.state}, ${restaurant.location.zip_code}`;
+    }
+    if (!hasUndefined(restaurant.phone)) {
+      result.phone = restaurant.phone;
+    }
+    if (!hasUndefined(restaurant.review_count)) {
+      result.review_count = restaurant.review_count;
+    }
+    // eslint-disable-next-line no-restricted-globals
+    if (!isNaN(restaurant.distance)) {
+      result.distance = `${(parseFloat(restaurant.distance) / 1000).toFixed(2)} km`;
+    }
+    if (!hasUndefined(restaurant.categories, restaurant.price)) {
+      result.categories += restaurant.price;
+      restaurant.categories.forEach((category) => {
+        result.categories += ` - ${category.title}`;
+      });
+    }
+    formatedResults.push(result);
+  });
+  return formatedResults;
+};
